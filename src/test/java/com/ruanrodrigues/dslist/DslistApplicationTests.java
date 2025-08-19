b6342472b6d0a5113d6305b151e2aa9a5ca96ca8
Ruanrodrigues20 <ruan.rodrigues.silvaa@gmail.com>
2025-08-18 22:18:37 -0300

    first commit

---
+++ src/test/java/com/ruanrodrigues/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruanrodrigues.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
